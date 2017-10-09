75e740eea3c1972421b833b8aec972ac69829e1a
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-10-09 15:56:27 -0400

    starting APIs

--- keys.js
+++ keys.js
@@ -1,10 +1,24 @@
+// Consumer Key (API Key)	T7X8fCNU4THq2iSqwucDOHBOB
+// // Consumer Secret (API Secret)	f7juJzWBymVbuWrhGtYVOmmqAE6I9aEwu5qLeAHJWhEz56fNiQ
+// Owner	kimberlyOSX
+// Owner ID	911272816783298561
+// Access Token	911272816783298561-N7s6Zg020DSB1xvD0XVixGn1x7ADgUG
+// Access Token Secret	8RhD8Nwb2dJ7Q7qk3I5BTeYFyVYnujRZize0bUKD4AqAC
+// Owner	kimberlyOSX
+// Owner ID	911272816783298561
+
+
+
+
+
+
 console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: '<input here>',
-    consumer_secret: '<input here>',
-    access_token_key: '<input here>',
-    access_token_secret: '<input here>',
+    consumer_key: 'T7X8fCNU4THq2iSqwucDOHBOB,
+    consumer_secret: 'f7juJzWBymVbuWrhGtYVOmmqAE6I9aEwu5qLeAHJWhEz56fNiQ',
+    access_token_key: '911272816783298561-N7s6Zg020DSB1xvD0XVixGn1x7ADgUG',
+    access_token_secret: '8RhD8Nwb2dJ7Q7qk3I5BTeYFyVYnujRZize0bUKD4AqAC',
 }
 
 module.exports = twitterKeys;
