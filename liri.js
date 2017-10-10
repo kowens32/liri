c777480abc80c9df108798ec8dd1c73e83cf3a76
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-10-10 09:51:52 -0400

    trying to create for loop to capture var

---
+++ liri.js
@@ -0,0 +1,9 @@
+var twitterKeys = require('./keys');
+
+for ( var key in twitterKeys) {
+    https://api.twitter.com/oauth2/token
+        https://api.twitter.com/oauth/request_token
+             https://api.twitter.com/oauth/authorize
+    https://api.twitter.com/oauth/access_token
+
+}
