9dd9a34445a5966ba4269afcc9c055552b5984fd
kimberly owens <kimberlyowens@MacBook-Pro-2.local>
2017-10-14 15:34:53 -0400

    adding spotify

--- keys.js
+++ keys.js
@@ -9,3 +9,9 @@ var twitterKeys = {
 }
 
 module.exports = twitterKeys;
+
+var spotifyKeys ={
+    client_id: 'd49310936c4546a9809e15076613bb2d',
+    client_secret_id: 'b4471fcc4ef44a1e90ef3e8ab0548c00'
+}
+module.exports = spotifyKeys;
--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var Twitter = require('twitter');
-var Spotify = require('node-spotify-api');
+var Spotify = require('spotify');
 var twitterKeys = require('./keys');
+var spotifyKeys = require('./keys');
 
 var stringInput1 = process.argv[2];
 var stringInput2 = process.argv[3];
@@ -11,6 +12,12 @@ switch(stringInput1) {
         break;
 }
 
+switch (stringInput2){
+    case 'spotify-this-song':
+        findSong();
+        break;
+}
+
 function bird () {
     var client = new Twitter(twitterKeys);
 
@@ -18,14 +25,20 @@ function bird () {
     client.get('statuses/user_timeline', params, function (error, tweets, response) {
         if (tweets) {
 
-            console.log(tweets[1].created_at);
+            console.log(tweets[1].length);
             tweets.for
 
         }
     });
 }
 
-// var music = new Spotify({
-//     id:
-// })
+function findSong () {
+    var music = new Spotify(spotifyKeys)
 
+    Spotify.search({tpe: 'track', query: stringInput2}, function (err, data) {
+        if (err) {
+            return console.log('Error occurred: ' + err);
+        }
+        console.log(data);
+    });
+}
