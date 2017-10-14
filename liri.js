b1f3aa49e9aade15e287e1d6a60bbf1a492013ed
kimberly owens <kimberlyowens@MacBook-Pro-2.local>
2017-10-14 19:30:20 -0400

    updated OMBD but nothing works yet

--- liri.js
+++ liri.js
@@ -1,10 +1,13 @@
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require('request');
 var twitterKeys = require('./keys');
 var spotifyKeys = require('./keys');
 
 var stringInput1 = process.argv[2];
 var stringInput2 = process.argv[3];
+var stringInput3 = process.argv[4];
+var movieName = "";
 
 switch(stringInput1) {
     case 'my-tweets':
@@ -43,3 +46,22 @@ function findSong () {
     });
 }
 
+for (var i = 4; i < stringInput3.length; i++) {
+    if ( i > 4 && i <stringInput3.length ) {
+        movieName = movieName + "+" + stringInput3[i];
+    }
+
+    else {
+        movieName += stringInput3[i];
+    }
+}
+
+var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=40e9cece";
+
+console.log(queryUrl);
+
+request(queryUrl, function (error, response, body){
+    if (!error && response.statusCode === 200) {
+        console.log('Release Year: ' + JSON.parse(body).Year);
+    }
+});
