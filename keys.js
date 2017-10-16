05fa2cab960f127d37c34f2d46125e722a4d1daa
kimberly owens <kimberlyowens@MacBook-Pro-2.attlocal.net>
2017-10-16 14:18:05 -0400

    fixed bug with OMDB

--- keys.js
+++ keys.js
@@ -2,8 +2,8 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-    consumer_key: 'T7X8fCNU4THq2iSqwucDOHBOB',
-    consumer_secret: 'f7juJzWBymVbuWrhGtYVOmmqAE6I9aEwu5qLeAHJWhEz56fNiQ',
+    consumer_key: 'E5yfqJLpmm5m1T82h67yFeYWa',
+    consumer_secret: 'fDpfWkhh5Geukx0JmnR8ydp39LFOua58J0kScgZvhjbb9ZREacN',
     access_token_key: '911272816783298561-N7s6Zg020DSB1xvD0XVixGn1x7ADgUG',
     access_token_secret: '8RhD8Nwb2dJ7Q7qk3I5BTeYFyVYnujRZize0bUKD4AqAC'
 }
--- liri.js
+++ liri.js
@@ -6,53 +6,56 @@ var spotifyKeys = require('./keys');
 
 var stringInput1 = process.argv[2];
 var stringInput2 = process.argv[3];
-var stringInput3 = process.argv[4];
+var stringInput3 = process.argv[3];
 var movieName = "";
 
-switch(stringInput1) {
-    case 'my-tweets':
-        bird();
-        break;
-}
+// switch(stringInput1) {
+//     case 'my-tweets':
+//         bird();
+//         break;
+// }
 
-switch (stringInput2){
-    case 'spotify-this-song':
-        findSong();
-        break;
-}
+// switch (stringInput2){
+//     case 'spotify-this-song':
+//         findSong();
+//         break;
+// }
 
-function bird () {
-    var client = new Twitter(twitterKeys);
-
-    var params = {screen_name: 'kimberlyOSX'};
-    client.get('statuses/user_timeline', params, function (error, tweets, response) {
-        if (tweets) {
-
-            console.log(tweets[1].length);
-            tweets.for
-
-        }
-    });
-}
-
-function findSong () {
-    var music = new Spotify(spotifyKeys)
-
-    music.search({tpe: 'track', query: stringInput2}, function (err, data) {
-        if (err) {
-            return console.log('Error occurred: ' + err);
-        }
-        console.log(data);
-    });
-}
+// function bird () {
+//     var client = new Twitter(twitterKeys);
+//
+//     var params = {screen_name: 'kimberlyOSX'};
+//     client.get('statuses/user_timeline', params, function (error, tweets, response) {
+//         if (tweets) {
+//
+//             console.log(tweets);
+//
+//         }
+//     });
+// }
+//
+// function findSong () {
+//     var music = new Spotify(spotifyKeys)
+//
+//     music.search({tpe: 'track', query: stringInput2}, function (err, data) {
+//         if (err) {
+//             return console.log('Error occurred: ' + err);
+//         }
+//         console.log(data);
+//     });
+// }
 
 
 var queryUrl = "http://www.omdbapi.com/?t=" +stringInput3+ "&y=&plot=short&apikey=40e9cece";
-
+console.log(stringInput3);
 console.log(queryUrl);
 
 request(queryUrl, function (error, response, body){
     if (!error && response.statusCode === 200) {
-        console.log('Release Year: ' + JSON.parse(body).Year);
+        console.log('Release Year: ' + JSON.parse(body).Year + ' Title: ' + JSON.parse(body).Title +
+            ' IMDB Rating: ' + JSON.parse(body).imdbRating +
+            ' Rotten Tomatoes Rating: ' + JSON.parse(body).Ratings[1].Value +
+            ' Country: ' + JSON.parse(body).Country +  ' Language: ' + JSON.parse(body).Language +
+            ' Quick Plot: ' + JSON.parse(body).Plot +  ' Actors: ' + JSON.parse(body).Actors);
     }
 });
